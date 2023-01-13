b973609508a53a6fabc19c217531fd3d269f30f1
orzorzooo <orzorzooo@gmail.com>
2023-01-13 11:27:23 +0800

    ADD: 全域錯誤通知

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -3,10 +3,21 @@
     <!-- <default-bar /> -->
 
     <default-view />
+    <v-snackbar
+      v-model="global.show"
+      multi-line
+      color="red"
+      timeout="1500"
+      class="text-center"
+    >
+      {{ global.text }}
+    </v-snackbar>
   </v-app>
 </template>
 
 <script setup>
+import { globalStore } from "@/stores/global";
 import DefaultBar from "./AppBar.vue";
 import DefaultView from "./View.vue";
+const global = globalStore();
 </script>
